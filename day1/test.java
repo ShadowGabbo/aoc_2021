8b94adc37744ad4edab2daf37fba73b6cfb8a856
Gabriele Sarti <72649485+ShadowGabbo@users.noreply.github.com>
2021-12-01 23:37:13 +0100

    Delete day1 directory

--- day1/test.java
+++
@@ -1,7 +0,0 @@
-public class test{
-    public static void main(String[] args) {
-        submarine s = new submarine();
-        s.insert("input.txt");
-        System.out.println("Counter: "+ s.counter());
-    }
-}
