fdde8e07810e3482ac75e50d60eb6bff1e64a046
Gabriele Sarti <72649485+ShadowGabbo@users.noreply.github.com>
2021-12-04 18:49:34 +0100

    Add part2

---
+++ day2/part2/test.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class test{
+    public static void main(String[] args) {
+        int h_pos=0,depth=0, aim=0;
+        ArrayList<Integer> elems = new ArrayList<Integer>();
+        ArrayList<String> cmds = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line,cmd;
+            while ((line = br.readLine()) != null) {
+               String cmd2 = line.replaceAll(" ", "");
+               cmd = cmd2.substring(0, cmd2.length() -1);
+               int elem = (int)(cmd2.charAt(cmd2.length()-1)-'0');
+               cmds.add(cmd);
+               elems.add(elem);
+               }
+               //System.out.println(cmds.toString());
+               //System.out.println(elems.toString());
+               for (int i = 0; i < elems.size(); i++) {
+                int elem = elems.get(i);
+                String command = cmds.get(i);
+                if (command.equals("forward")) {
+                    h_pos+=elem;
+                    depth+=(aim*elem);
+                }
+                if (command.equals("down")) {
+                    aim+=elem;
+                }
+                if (command.equals("up")) {
+                    aim-=elem;
+                }
+                System.out.println(command);
+                System.out.println(elem);
+            }
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("h_pos ha valore: "+h_pos+ ", depth ha valore: "+depth+", totale: "+h_pos*depth);
+    }
+    
+}
