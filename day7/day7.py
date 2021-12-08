7fc715dd6e05149b8700ccb7a9892aa71e437256
Gabriele Sarti <72649485+ShadowGabbo@users.noreply.github.com>
2021-12-08 13:03:33 +0100

    Fixed description

---
+++ day7/day7.py
@@ -0,0 +1,18 @@
+from statistics import median, mean
+
+input = [int(i) for i in open('input.txt').read().strip().split(',')]
+low, high = min(input), max(input)
+
+# part 1
+target = int(median(input))
+fuel = sum([abs(target - pos) for pos in input])
+print('Part #1:', fuel)
+
+# part 2
+target = int(mean(input))
+fuel = int(min([
+    sum(abs(t - pos) * (abs(t - pos) + 1) / 2 for pos in input)
+    for t in [target, target + 1]
+]))
+
+print('Part #2:', fuel)
